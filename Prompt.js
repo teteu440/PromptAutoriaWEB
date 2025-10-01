93c321b6bcbadf54884a1afd357f681f2b2d3426
teteu440 <franca.matheus@escolar.ifrn.edu.br>
2025-10-01 18:53:30 -0300

    Add files via upload

---
+++ Prompt.js
@@ -0,0 +1,12 @@
+
+function mostrarPrompt (){
+    let name = prompt("Como poderia lhe ajudar?")
+    if (name !== null && name !==""){
+         document.getElementById("resultado").innerHTML = "Olá, " + nome + "!";
+    }else{
+        document.getElementById("resultado").innerHTML = "Você não digitou um nome.";
+    }
+}
+
+
+
