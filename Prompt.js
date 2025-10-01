dfafacc788493dbb7d3ae83c57411c49604e5ff8
LucasMatheus <franca.matheus@escolar.ifrn.edu.br>
2025-10-01 20:20:03 -0300

    Add files via upload

--- Prompt.js
+++ Prompt.js
@@ -2,7 +2,7 @@
 function mostrarPrompt (){
     let name = prompt("Como poderia lhe ajudar?")
     if (name !== null && name !==""){
-         document.getElementById("resultado").innerHTML = "Olá, " + nome + "!";
+        document.getElementById("resultado").innerHTML = "Olá, " + nome + "!";
     }else{
         document.getElementById("resultado").innerHTML = "Você não digitou um nome.";
     }
